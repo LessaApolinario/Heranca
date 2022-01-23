790b47835ef99b4c1f0bc30567536b19c3a683de
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-23 20:16:58 -0300

    Add subclass Cliente and its methods

---
+++ src/Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente extends Pessoa {
+
+    private double saldo;
+
+    /**
+     * A subclasse herda o estado e os comportamentos da superclasse direta.
+     * Isso significa que o construtor da superclasse e os seus métodos
+     * também serão acessíveis na subclasse.
+     */
+    public Cliente(String nome, String cpf, String dataNascimento, double saldo) {
+        super(nome, cpf, dataNascimento);
+
+        if (saldo >= 0) {
+            this.saldo = saldo;
+        }
+    }
+}
