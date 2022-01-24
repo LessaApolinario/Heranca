315dcfb3ac93df79f6edbcab56f6b3b61e7204ec
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-24 12:30:17 -0300

    Add cadastrar casa into menu

--- src/Main.java
+++ src/Main.java
@@ -115,10 +115,11 @@ public class Main {
     public static void menu() {
         System.out.println("1. Cadastrar cliente");
         System.out.println("2. Cadastrar vendedor de imóveis");
-        System.out.println("3. Obter informações do cliente");
-        System.out.println("4. Obter informações do vendedor de imóveis");
-        System.out.println("5. Vender casa");
-        System.out.println("6. Listar casas disponíveis");
+        System.out.println("3. Cadastrar casa");
+        System.out.println("4. Obter informações do cliente");
+        System.out.println("5. Obter informações do vendedor de imóveis");
+        System.out.println("6. Vender casa");
+        System.out.println("7. Listar casas disponíveis");
         System.out.println("0. Sair");
     }
 
