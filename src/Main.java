8863aa32693523b835890be6fdc69fa81ab874e3
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-23 21:11:25 -0300

    Add dialog menu

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void menu() {
+        System.out.println("1. Cadastrar cliente");
+        System.out.println("2. Cadastrar vendedor de imóveis");
+        System.out.println("3. Obter informações do cliente");
+        System.out.println("4. Obter informações do vendedor de imóveis");
+        System.out.println("5. Vender casa");
+        System.out.println("6. Listar casas disponíveis");
+        System.out.println("0. Sair");
+    }
 }
