d94ace49a9f153282413575e30d53b19999843ae
Lessa Apolinario <lessaapolinario@gmail.com>
2022-01-23 21:26:45 -0300

    Fix: constructor

--- src/GerenciadorDePessoas.java
+++ src/GerenciadorDePessoas.java
@@ -5,7 +5,7 @@ public class GerenciadorDePessoas {
     private ArrayList<Cliente> clientes;
     private ArrayList<VendedorDeImoveis> vendedores;
 
-    public GerenciadorDePessoas(ArrayList<Cliente> clientes, ArrayList<VendedorDeImoveis> vendedores) {
+    public GerenciadorDePessoas() {
         this.clientes = new ArrayList<>();
         this.vendedores = new ArrayList<>();
     }
@@ -13,11 +13,15 @@ public class GerenciadorDePessoas {
     public Cliente cadastrarCliente(String nome, String cpf, String dataNascimento, double saldo) {
         Cliente c = new Cliente(nome, cpf, dataNascimento, saldo);
 
+        clientes.add(c);
+
         return c;
     }
 
     public VendedorDeImoveis cadastrarVendedorDeImoveis(String nome, String cpf, String dataNascimento) {
         VendedorDeImoveis vendedorDeImoveis = new VendedorDeImoveis(nome, cpf, dataNascimento);
+
+        vendedores.add(vendedorDeImoveis);
 
         return vendedorDeImoveis;
     }
